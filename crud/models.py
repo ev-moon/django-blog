829caddb602604e90fa82c6cc5d1ecb1925c21f9
ev-moon <xenos96111@gmail.com>
2020-07-14 23:06:00 +0900

    first commit

---
+++ crud/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    create_at = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.title
