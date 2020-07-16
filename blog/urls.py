b1c9621406de5b9256038c3040ef3637bfb49db9
ev-moon <xenos96111@gmail.com>
2020-07-16 16:48:26 +0900

    feat: image portfolio

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from django.urls import path
 import crud.views
+import portfolio.views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', crud.views.home, name='home'),
     path('blog/<int:blog_id>', crud.views.detail, name='detail'),
     path('blog/new', crud.views.new, name='new'),
-    path('blog/create', crud.views.create, name='create')
-]
+    path('blog/create', crud.views.create, name='create'),
+    path('portfolio', portfolio.views.portfolio, name='portfolio'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
