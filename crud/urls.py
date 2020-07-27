0abc3a836ca9fd6d00d5f5eca069be246d3a1160
ev-moon <xenos96111@gmail.com>
2020-07-27 12:29:05 +0900

    style: applied bootstrap album theme

--- crud/urls.py
+++ crud/urls.py
@@ -3,6 +3,6 @@ import crud.views
 
 urlpatterns = [
     path('<int:blog_id>', crud.views.detail, name='detail'),
-    path('new', crud.views.new, name='new'),
+    path('new/', crud.views.new, name='new'),
     path('create', crud.views.create, name='create'),
 ]
